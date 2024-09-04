10f2743d29664356697f70c48028aa746d975107
salamaxx97 <mohamedsalama20072@gmail.com>
2024-09-04 16:33:13 +0300

    instances  resources added

--- ec2.tf
+++ ec2.tf
@@ -49,3 +49,49 @@ resource "aws_vpc_security_group_egress_rule" "privaate_allow_outbound" {
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
+# Public EC2 instance with Nginx
+resource "aws_instance" " public" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  security_groups        = [aws_security_group.public.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y nginx
+              rm /etc/nginx/sites-enabled/default
+              echo 'server {
+                      listen 80;
+                      location / {
+                          proxy_pass http://${aws_instance.apache_private.private_ip};
+                      }
+              }' > /etc/nginx/sites-available/default
+              ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+              systemctl restart nginx
+              EOF
+
+  tags = {
+    Name = "NginxPublicInstance"
+  }
+}
+
+# Private EC2 instance with Apache
+resource "aws_instance" "private" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.private.id
+  security_groups        = [aws_security_group.private.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              echo "Hello from $(hostname)" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
+
+  tags = {
+    Name = "ApachePrivateInstance"
+  }
+}
