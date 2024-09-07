88161d9b055810486712759921f2e4ec8fdbf588
salamaxx97 <mohamedsalama20072@gmail.com>
2024-09-07 13:03:59 +0300

    vpc.tf using maps and lists

--- vpc.tf
+++ vpc.tf
@@ -4,24 +4,18 @@ resource "aws_vpc" "main" {
     Name = "TerraformLabVPC"
   }
 }
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnet_cidr
-  map_public_ip_on_launch = true
-  availability_zone = var.public_availability_zone
+resource "aws_subnet" "subnets" {
+  for_each                = var.subnet_cidrs
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = each.value[0]
+  map_public_ip_on_launch = each.value[1]
+  availability_zone       = var.availability_zones[0]
   tags = {
-    Name = "PublicSubnet"
+    Name = each.key
   }
 }
 
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnet_cidr
-  availability_zone = var.private_availability_zone
-  tags = {
-    Name = "PrivateSubnet"
-  }
-}
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = {
@@ -40,18 +34,15 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
 
 resource "aws_eip" "nat_eip" {
-  domain   = "vpc"
+  domain = "vpc"
 }
+
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = aws_subnet.subnets["public"].id
   tags = {
     Name = "NatGateway"
   }
@@ -68,9 +59,9 @@ resource "aws_route_table" "private" {
   }
 }
 
-resource "aws_route_table_association" "private_assoc" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
+resource "aws_route_table_association" "assoc" {
+  for_each       = var.subnet_cidrs
+  subnet_id      = aws_subnet.subnets[each.key].id
+  route_table_id = each.key == "public" ? aws_route_table.public.id : aws_route_table.private.id
+
 }
-
-
