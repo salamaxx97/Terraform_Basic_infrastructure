538409d1fc1f235140f1c8f2bfa6fd73d2330596
salamaxx97 <mohamedsalama20072@gmail.com>
2024-09-07 13:30:21 +0300

    SG made using maps and lists

--- ec2.tf
+++ ec2.tf
@@ -1,97 +1,109 @@
-resource "aws_security_group" "public" {
-  name        = "allow_public_http"
-  description = "Allow http inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
+resource "aws_security_group" "SG" {
+  for_each = var.security_groups
+  name     = each.value
+  vpc_id   = aws_vpc.main.id
 
   tags = {
-    Name = "allow_http"
+    Name = each.value
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_public_http" {
-  security_group_id = aws_security_group.public.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-}
+# Ingress rule for public security group to allow HTTP traffic from anywhere
+resource "aws_security_group_rule" "public_ingress_http" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 
+  security_group_id = aws_security_group.SG["public"].id
 
-resource "aws_vpc_security_group_egress_rule" "public_allow_outbounds" {
-  security_group_id = aws_security_group.public.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
-resource "aws_security_group" "private" {
-  name        = "allow_private_http"
-  description = "Allow http inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  tags = {
-    Name = "allow_private_http"
+  lifecycle {
+    create_before_destroy = true
   }
+
+  depends_on = [aws_security_group.SG]
 }
 
-resource "aws_security_group_rule" "private_http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  source_security_group_id  = aws_security_group.public.id
-  security_group_id = aws_security_group.private.id
-}
+# Ingress rule for private security group to allow HTTP traffic from public security group
+resource "aws_security_group_rule" "private_ingress_http" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.SG["public"].id
 
+  security_group_id = aws_security_group.SG["private"].id
 
-resource "aws_vpc_security_group_egress_rule" "privaate_allow_outbound" {
-  security_group_id = aws_security_group.private.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
-# Public EC2 instance with Nginx
-resource "aws_instance" "public" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  subnet_id              = aws_subnet.public.id
-  security_groups        = [aws_security_group.public.id]
-  associate_public_ip_address = true
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y nginx
-              rm /etc/nginx/sites-enabled/default
-              echo 'server {
-                      listen 80;
-                      location / {
-                          proxy_pass http://${aws_instance.private.private_ip};
-                      }
-              }' > /etc/nginx/sites-available/default
-              ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
-              systemctl restart nginx
-              EOF
-
-  tags = {
-    Name = "NginxPublicInstance"
+  lifecycle {
+    create_before_destroy = true
   }
+
+  depends_on = [aws_security_group.SG]
 }
 
-# Private EC2 instance with Apache
-resource "aws_instance" "private" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  subnet_id              = aws_subnet.private.id
-  security_groups        = [aws_security_group.private.id]
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              echo "Hello from $(hostname)" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
+# Egress rule for all security groups to allow all outbound traffic
+resource "aws_security_group_rule" "all_egress" {
+  for_each    = var.security_groups
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1" # Allows all protocols
+  cidr_blocks = ["0.0.0.0/0"]
 
-  tags = {
-    Name = "ApachePrivateInstance"
+  security_group_id = aws_security_group.SG[each.key].id
+
+  lifecycle {
+    create_before_destroy = true
   }
+
+  depends_on = [aws_security_group.SG]
 }
+
+
+# # Public EC2 instance with Nginx
+# resource "aws_instance" "public" {
+#   ami                    = var.ami
+#   instance_type          = var.instance_type
+#   subnet_id              = aws_subnet.public.id
+#   security_groups        = [aws_security_group.public.id]
+#   associate_public_ip_address = true
+
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               apt-get update
+#               apt-get install -y nginx
+#               rm /etc/nginx/sites-enabled/default
+#               echo 'server {
+#                       listen 80;
+#                       location / {
+#                           proxy_pass http://${aws_instance.private.private_ip};
+#                       }
+#               }' > /etc/nginx/sites-available/default
+#               ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+#               systemctl restart nginx
+#               EOF
+
+#   tags = {
+#     Name = "NginxPublicInstance"
+#   }
+# }
+
+# # Private EC2 instance with Apache
+# resource "aws_instance" "private" {
+#   ami                    = var.ami
+#   instance_type          = var.instance_type
+#   subnet_id              = aws_subnet.private.id
+#   security_groups        = [aws_security_group.private.id]
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               apt-get update
+#               apt-get install -y a pache2
+#               echo "Hello from $(hostname)" > /var/www/html/index.html
+#               systemctl restart apache2
+#               EOF
+
+#   tags = {
+#     Name = "ApachePrivateInstance"
+#   }
+# }
